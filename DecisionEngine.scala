86da27052f15bf46f0735e37a0663f17e33b292f
IMTorg <information@mgmt-tech.org>
2018-08-15 00:18:52 -0400

    mod changes for notebooks

---
+++ DecisionEngine.scala
@@ -0,0 +1,9 @@
+package com.whitehawk.decisionengine
+
+object DecisionEngine {
+  def main(args: Array[String]) = println("hello")
+}
+
+class DecisionEngine {
+  def doSomething(value: Int): Int = value * 2
+}
